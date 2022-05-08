a8a4c212e8383ddfb16335e216850e7a8333751b
Romuru <pastelblue0721@gmail.com>
2022-05-08 23:56:18 +0900

    Selectors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./components/atom";
 function App() {
-  return <div>null</div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
 }
 
 export default App;
